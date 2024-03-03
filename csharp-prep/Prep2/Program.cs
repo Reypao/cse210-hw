31bcced00ec8abdd0d2672d92b74202f1c1d5c24
Reypao <efrey5guzman@gmail.com>
2024-03-02 22:33:38 -0700

    change1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+        int percUser = int.Parse(userGrade);
+
+        string letter = "";
+        string sign = ""; // Variable to hold the sign (+, -, or nothing)
+
+        // Determine the letter grade
+        if (percUser >= 90)
+        {
+            letter = "A";
+        }
+        else if (percUser >= 80)
+        {
+            letter = "B";
+        }
+        else if (percUser >= 70)
+        {
+            letter = "C";
+        }
+        else if (percUser >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine the sign based on the last digit
+        int lastDigit = percUser % 10;
+        if (percUser >= 60 && percUser < 90) // Exclude A and F grades from having signs
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        // Special case handling: There is no A+ or F+ and F-
+        if (letter == "A" && sign == "+")
+        {
+            sign = ""; // A+ doesn't exist, revert to A
+        }
+        else if (letter == "F")
+        {
+            sign = ""; // F+ and F- don't exist, revert to F
+        }
+
+    Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (percUser >= 70)
+        {
+            Console.WriteLine("Congratulations!. You approved!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            // Generating a random number between 1 and 100
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+            int guess = 0;
+            int numberOfGuesses = 0;
+
+            Console.WriteLine("I have chosen a number between 1 and 100. Try to guess it!");
+
+            // Loop until the user guesses the magic number
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                numberOfGuesses++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {numberOfGuesses} guesses!");
+                }
+            }
+
+            // Asking to the user if they want to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            string userResponse = Console.ReadLine().ToLower();
+            playAgain = userResponse == "yes";
+        }
     }
 }
