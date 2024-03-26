58e97c3b293f5de15ac53544ff594ed3f90b18d3
Reypao <efrey5guzman@gmail.com>
2024-03-25 22:49:42 -0600

    shapesA

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Polymorphism
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning05 World!");
+        static void Main(string[] args)
+        {
+
+            List<Shape> shapes = new List<Shape>();
+
+            Square sq = new Square("Yellow", 3);
+            shapes.Add(sq);
+
+            Rectangle rec = new Rectangle("Red", 4, 5);
+            shapes.Add(rec);
+
+            Circle cir = new Circle("Pink", 6);
+            shapes.Add(cir);
+
+            foreach (Shape s in shapes)
+            {
+                string color = s.GetColor();
+
+                double area = s.GetArea();
+
+                Console.WriteLine("The "+ color + " shape has an area of " + area + ".");
+           
+            }
+            Console.ReadKey();
+        }
     }
 }
